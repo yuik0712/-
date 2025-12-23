f32d5bad2fcd58b4899ab89099b3c7d2c2e74212
yuik <163374559+yuik0712@users.noreply.github.com>
2025-12-24 02:15:18 +0900

    [feat] spring jwt-auth 인증권한 예제 코드 파일 커밋
    velog bum12ark.log님의 코드를 보며 같이 구현하였습니다.

---
+++ backend-spring/bum12ark/jwt-auth/src/test/java/com/bum12ark/jwt_auth/JwtAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bum12ark.jwt_auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
