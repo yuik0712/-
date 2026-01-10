0e5f354e2988ae15258c66f98cc1169802f3990d
yuik <163374559+yuik0712@users.noreply.github.com>
2026-01-10 16:56:42 +0900

    [feat] UserDetailService 구현 및 사용자 인증 로직 추가

---
+++ backend-spring/bum12ark/jwt-auth/user-service/src/main/java/com/awt_auth/user_service/domain/service/UserServiceImpl.java
@@ -0,0 +1,31 @@
+package com.awt_auth.user_service.domain.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+@Slf4j
+public class UserServiceImpl implements UserService, UserDetailsService{
+    private final CustomRepository customRepository;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found in the database"));
+
+        Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority(user.getDtype()));
+        return new org.springframework.security.core.userdetails.User(user.getId().toString(), user.getPassword(), authorities);
+    }
+}
