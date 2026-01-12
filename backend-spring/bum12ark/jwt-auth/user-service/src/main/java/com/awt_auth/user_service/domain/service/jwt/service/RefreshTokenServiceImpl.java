838595033ec8221e162449c2118ec5bfb814e24a
yuik <163374559+yuik0712@users.noreply.github.com>
2026-01-12 14:01:20 +0900

    feat: Redis를 활용한 Refresh Token 저장 및 갱신 로직 구현

---
+++ backend-spring/bum12ark/jwt-auth/user-service/src/main/java/com/awt_auth/user_service/domain/service/jwt/service/RefreshTokenServiceImpl.java
@@ -0,0 +1,19 @@
+package com.awt_auth.user_service.domain.service.jwt.service;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+@Slf4j
+public class RefreshTokenServiceImpl implements RefreshTokenService{
+    private final UserRepository userRepository;
+    private final RefreshTokenRedisRepository refreshTokenRedisRepository;
+
+    @Transactional
+    @Override
+    public void updateRefreshToken(Long id, String uuid) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new NotExistUserException("사용자 고유번호: " + id + "는 없는 사용자입니다."));
+
+        refreshTokenRedisRepository.save(RefreshToken.of(user.getId().toString(), uuid));
+    }
+}
